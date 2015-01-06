6ca021a82b15e22fa246e8a1ad7146a07ac633d8
JoeCodex <joecodex10@gmail.com>
2015-01-06 12:24:51 -0500

    add edit  & update options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  
+  before_action :logged_in_user, only: [:edit, :update]
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 
   def show
     @user = User.find(params[:id])
@@ -9,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -19,12 +29,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-      :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+    :password_confirmation)
+  end
 end
